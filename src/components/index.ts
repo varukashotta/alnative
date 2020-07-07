4eac7753c8f67aa72da78a97dd18acda50772961
Waruka Shotta <alkashotta@gmail.com>
2020-07-08 00:40:39 +0800

    fix(onboarding): exporting components to be used as libraries
    BREAKING CHANGE: Modules need to be exported to be built as a library

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,7 @@
 export {default as Button} from './button';
+export {default as Onboarding} from './onboarding';
+export {  widthPercentageToDP,
+    heightPercentageToDP,
+    objectChecker,
+    truncate,
+    urlChecker} from './utils';
